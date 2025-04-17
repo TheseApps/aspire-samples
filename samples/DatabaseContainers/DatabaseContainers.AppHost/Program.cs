e1b22d31dd13be7af18c50dfe5f6224ee551ea40
jeff <jeff@theseapps.net>
2025-04-17 03:52:48 -0400

    z

--- samples/DatabaseContainers/DatabaseContainers.AppHost/Program.cs
+++ samples/DatabaseContainers/DatabaseContainers.AppHost/Program.cs
@@ -2,21 +2,21 @@
 
 // PostgreSQL container is configured with an auto-generated password by default
 // and supports setting the default database name via an environment variable & running *.sql/*.sh scripts in a bind mount.
-var todosDbName = "Todos";
+//UNDOPOSTGRES: var todosDbName = "Todos";
 
-var postgres = builder.AddPostgres("postgres")
-    // Set the name of the default database to auto-create on container startup.
-    .WithEnvironment("POSTGRES_DB", todosDbName)
-    // Mount the SQL scripts directory into the container so that the init scripts run.
-    .WithBindMount("../DatabaseContainers.ApiService/data/postgres", "/docker-entrypoint-initdb.d")
-    // Configure the container to store data in a volume so that it persists across instances.
-    .WithDataVolume()
-    .WithPgWeb()
-    // Keep the container running between app host sessions.
-    .WithLifetime(ContainerLifetime.Persistent);
+//UNDOPOSTGRES: var postgres = builder.AddPostgres("postgres")
+// Set the name of the default database to auto-create on container startup.
+//UNDOPOSTGRES: .WithEnvironment("POSTGRES_DB", todosDbName)
+// Mount the SQL scripts directory into the container so that the init scripts run.
+//UNDOPOSTGRES: .WithBindMount("../DatabaseContainers.ApiService/data/postgres", "/docker-entrypoint-initdb.d")
+// Configure the container to store data in a volume so that it persists across instances.
+//UNDOPOSTGRES: .WithDataVolume()
+//UNDOPOSTGRES: .WithPgWeb()
+// Keep the container running between app host sessions.
+//UNDOPOSTGRES: .WithLifetime(ContainerLifetime.Persistent);
 
 // Add the default database to the application model so that it can be referenced by other resources.
-var todosDb = postgres.AddDatabase(todosDbName);
+//UNDOPOSTGRES: var todosDb = postgres.AddDatabase(todosDbName);
 
 // MySql container is configured with an auto-generated password by default
 // and supports setting the default database name via an environment variable & running *.sql/*.sh scripts in a bind mount.
@@ -49,8 +49,8 @@ var addressBookDb = sqlserver.AddDatabase("AddressBook")
     .WithCreationScript(File.ReadAllText(initScriptPath));
 
 builder.AddProject<Projects.DatabaseContainers_ApiService>("apiservice")
-    .WithReference(todosDb)
-    .WaitFor(todosDb)
+    //UNDOPOSTGRES: .WithReference(todosDb)
+    //UNDOPOSTGRES: .WaitFor(todosDb)
     .WithReference(catalogDb)
     .WaitFor(catalogDb)
     .WithReference(addressBookDb)
